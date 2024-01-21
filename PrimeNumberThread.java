fb1ccdd5b0c041c37364911f8c146febc862247b
erik <erisandv@stud.ntnu.no>
2024-01-21 16:45:18 +0100

    Fifth Commit Commenting code

--- PrimeNumberThread.java
+++ PrimeNumberThread.java
@@ -5,6 +5,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+//A class that represents a thread that looks for prime numbers between a certain range
 public class PrimeNumberThread implements Runnable {
     private final int start;
     private final int end;
@@ -20,6 +21,7 @@ public class PrimeNumberThread implements Runnable {
     public void run() {
         for (int i = start; i <= end; i++) {
             if (isPrime(i)) {
+                //Synchronized to avoid concurrency issues
                 synchronized (primeNumbers) {
                     primeNumbers.add(i);
                 }
@@ -27,6 +29,7 @@ public class PrimeNumberThread implements Runnable {
         }
     }
 
+    //Checks if number is prime
     private boolean isPrime(int num)
     {
         if(num <= 1) {
@@ -42,12 +45,15 @@ public class PrimeNumberThread implements Runnable {
 
 }
 
+//Class that finds primes between a range using a given number of threads
 class PrimeFinder {
     public static List<Integer> findPrimes(int start, int end, int numOfThreads) {
         List<Integer> primeNumbers = new ArrayList<>();
 
+        //Makes a fixedThreadPool with a number of threads equal to the integer "numOfThreads"
         ExecutorService executorService = Executors.newFixedThreadPool(numOfThreads);
 
+        //Divides the work into numOfThreads-parts
         int partStart;
         int partEnd;
         int rangeSize = (end - start + 1) / numOfThreads;
@@ -60,30 +66,33 @@ class PrimeFinder {
                 partEnd = partStart + rangeSize - 1;
             }
 
+            //Makes the thread
             executorService.submit(new PrimeNumberThread(partStart, partEnd, primeNumbers));
         }
 
-
+        //Shutdown the ExecutorService
         executorService.shutdown();
 
+        //Waits for all threads to finish
         try {
             executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
+        //Sort the prime numbers in ascending order
         primeNumbers.sort(Comparator.naturalOrder());
 
         return primeNumbers;
-
     }
-
-
 }
+
 
 class RunThreads {
     public static void main(String[] args) {
+        //Finds all primes between 0 and 100 using 3 threads
         List<Integer> primes = PrimeFinder.findPrimes(0, 100, 3);
+
         System.out.println(primes);
     }
 
